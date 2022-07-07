d2f892973483ddf75cabf69fc039c4dd490c9009
Marina San Jose Pena <2569221S@student.gla.ac.uk>
2022-07-07 12:30:57 +0200

    Add event listeners for mobile devices

--- script.js
+++ script.js
@@ -25,8 +25,19 @@ canvas.width = 1000;
 canvas.style.left = "300px";
 canvas.style.top = "100px";
 
+const isMobile = window.matchMedia("only screen and (max-width: 700px)").matches;
+
+if (isMobile) {
+  canvas.height = 600;
+  canvas.width = 450;
+  canvas.style.left = "20px";
+  canvas.style.top = "30px";
+}
+
 const context = canvas.getContext('2d');
 
+
+// for computer
 canvas.addEventListener('mousedown', e => {
   x = e.offsetX;
   y = e.offsetY;
@@ -49,6 +60,32 @@ window.addEventListener('mouseup', e => {
     isDrawing = false;
   }
 });
+
+
+//for mobile
+canvas.addEventListener('touchstart', e => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener('touchmove', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+window.addEventListener('touchend', e => {
+  if (isDrawing === true) {
+    drawLine(context, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
 
 function drawLine(context, x1, y1, x2, y2) {
   context.beginPath();
